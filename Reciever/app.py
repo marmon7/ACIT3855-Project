74b8e44e992f68eca3899983a88be93c1d0c7d1a
Brandon <bbarnett15@my.bcit.ca>
2025-04-10 16:13:26 -0700

    updated reciever to use kafkawrapper

--- Reciever/app.py
+++ Reciever/app.py
@@ -7,7 +7,7 @@ import logging.config
 import yaml
 import connexion
 from connexion import NoContent
-from pykafka import KafkaClient
+from kafka_client import KafkaWrapper
 
 with open('app_conf.yaml','r', encoding="utf-8") as f:
     app_config = yaml.safe_load(f.read())
@@ -17,20 +17,19 @@ with open("log_conf.yaml", "r", encoding="utf-8") as f:
     logging.config.dictConfig(LOG_CONFIG)
 logger = logging.getLogger('basicLogger')
 
+kafka_wrapper = KafkaWrapper(f"{app_config["events"]["hostname"]}:{app_config["events"]["port"]}",app_config["events"]["topic"].encode())
+
 def book_beach_activity(body):
     """Sends Payload to kafka for a beach activity event"""
     body["trace_id"] = time.time_ns()
     logger.info("Received event beach_activity with a trace id of %s", body["trace_id"])
-    client = KafkaClient(hosts=f"{app_config["events"]["hostname"]}:{app_config["events"]["port"]}")
-    topic = client.topics[str.encode(app_config["events"]["topic"])]
-    producer = topic.get_sync_producer()
     msg = {
         "type": "beachactivity",
         "datetime": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
         "payload": body
     }
     msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
+    kafka_wrapper.producer_msg(msg_str.encode('utf-8'))
     logger.info("Response for event beach_activity (id: %s) has status 201", body["trace_id"])
     return NoContent, 201
 
@@ -38,16 +37,13 @@ def report_beach_conditions(body):
     """Send Payload to kafka for a beach condition"""
     body["trace_id"] = time.time_ns()
     logger.info("Received event beach_conditions with a trace id of %s", body["trace_id"])
-    client = KafkaClient(hosts=f"{app_config["events"]["hostname"]}:{app_config["events"]["port"]}")
-    topic = client.topics[str.encode(app_config["events"]["topic"])]
-    producer = topic.get_sync_producer()
     msg = {
         "type": "beachcondition",
         "datetime": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
         "payload": body
     }
     msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
+    kafka_wrapper.producer_msg(msg_str.encode('utf-8'))
     logger.info("Response for event beach_weather (id: %s) has status 201", body["trace_id"])
     return NoContent, 201
 
---
+++ Reciever/kafka_client.py
@@ -0,0 +1,111 @@
+import time
+import random
+import logging
+import logging.config
+import yaml
+from pykafka import KafkaClient
+from pykafka.exceptions import KafkaException
+from pykafka.common import OffsetType
+
+with open("log_conf.yaml", "r", encoding="utf-8") as f:
+    LOG_CONFIG = yaml.safe_load(f.read())
+    logging.config.dictConfig(LOG_CONFIG)
+logger = logging.getLogger('basicLogger')
+
+class KafkaWrapper:
+    def __init__(self, hostname, topic):
+        self.hostname = hostname
+        self.topic = topic
+        self.client = None
+        self.consumer = None
+        self.producer = None
+        self.connect()
+
+    def connect(self):
+        """Infinite loop: will keep trying"""
+        while True:
+            logger.debug("Trying to connect to Kafka...")
+            if self.make_client():
+                if self.make_consumer():
+                    break
+            # Sleeps for a random amount of time (0.5 to 1.5s)
+            time.sleep(random.randint(500, 1500) / 1000)
+
+    def make_client(self):
+        """
+        Runs once, makes a client and sets it on the instance.
+        Returns: True (success), False (failure)
+        """
+        if self.client is not None:
+            return True
+        try:
+            self.client = KafkaClient(hosts=self.hostname)
+            logger.info("Kafka client created!")
+            return True
+        except KafkaException as e:
+            msg = f"Kafka error when making client: {e}"
+            logger.warning(msg)
+            self.client = None
+            self.consumer = None
+            return False
+        
+    def make_consumer(self):
+        """
+        Runs once, makes a consumer and sets it on the instance.
+        Returns: True (success), False (failure)
+        """
+        if self.consumer is not None:
+            return True
+        if self.client is None:
+            return False
+        try:
+            topic = self.client.topics[self.topic]
+            self.consumer = topic.get_simple_consumer(
+                consumer_group=topic,
+                reset_offset_on_start=False,
+                auto_offset_reset=OffsetType.LATEST
+            )
+        except KafkaException as e:
+            msg = f"Make error when making consumer: {e}"
+            logger.warning(msg)
+            self.client = None
+            self.consumer = None
+            return False
+        
+    def messages(self):
+        """Generator method that catches exceptions in the consumer loop"""
+        if self.consumer is None:
+            self.connect()
+        while True:
+            try:
+                for msg in self.consumer:
+                    yield msg
+            except KafkaException as e:
+                msg = f"Kafka issue in comsumer: {e}"
+                logger.warning(msg)
+                self.client = None
+                self.consumer = None
+                self.connect()
+
+    def make_producer(self):
+        """
+        Runs once, makes a producer and sets it on the instance.
+        Returns: True (success), False (failure)
+        """
+        if self.consumer is not None:
+            return True
+        if self.client is None:
+            return False
+        try:
+            topic = self.client.topics[self.topic]
+            self.producer = topic.get_sync_producer()
+
+        except KafkaException as e:
+            msg = f"Make error when making consumer: {e}"
+            logger.warning(msg)
+            self.client = None
+            self.producer = None
+            return False
+    
+    def producer_msg(self,msg):
+        self.producer.produce(msg)
